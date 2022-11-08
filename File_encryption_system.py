f20104491af246d14d354d6387764cdcbb0808c7
oof1718963 <75444840+oof1718963@users.noreply.github.com>
2022-11-08 09:17:12 -0500

    Update File_encryption_system.py

--- File_encryption_system.py
+++ File_encryption_system.py
@@ -1,20 +1,32 @@
-from tkinter import *
-from tkinter import filedialog as fd
-import hashlib
 
-root=Tk()
-root.geometry("250x190")
+from cryptography.fernet import Fernet
 
-def apply_md5():
-    print("MD5 function")
-    
-    
-def apply_sha256():
-    print("SHA function")   
-    
-    
-btn=Button(root, text="Apply MD5",command=apply_md5)
-btn.place(relx=0.3,rely=0.5, anchor=CENTER)
-btn1=Button(root, text="Apply SHA256",command=apply_sha256)
-btn1.place(relx=0.7,rely=0.5, anchor=CENTER)
-root.mainloop()
+key = Fernet.generate_key()
+
+with open('mykey.key', 'wb') as mykey:
+    mykey.write(key)
+
+    with open('mykey.key', 'rb') as mykey:
+        key = mykey.read()
+
+    print(key)
+
+f = Fernet(key)
+
+with open('grades.csv', 'rb') as original_file:
+    original = original_file.read()
+
+encrypted = f.encrypt(original)
+
+with open ('enc_grades.csv', 'wb') as encrypted_file:
+    encrypted_file.write(encrypted)
+
+f = Fernet(key)
+
+with open('enc_grades.csv', 'rb') as encrypted_file:
+    encrypted = encrypted_file.read()
+
+decrypted = f.decrypt(encrypted)
+
+with open('dec_grades.csv', 'wb') as decrypted_file:
+    decrypted_file.write(decrypted)
